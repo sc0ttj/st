59cf42e777dfd2d6b812e9ad223b516a7ba6eb86
noname <email@example.com>
2020-11-05 21:04:38 +0000

    added some patches, builds OK in Puppy Linux Fossa64 (Focal Ubuntu based)

--- config.def.h
+++ config.def.h
@@ -5,7 +5,14 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "DejaVuSansMono Nerd Font:pixelsize=14:antialias=true:autohint=true";
+
+/* Spare fonts */
+static char *font2[] = {
+	"DejaVuSansMono Nerd Font:pixelsize=14:antialias=true:autohint=true",
+	"UbuntuMono Nerd Font:pixelsize=14:antialias=true:autohint=true",
+};
+
 static int borderpx = 2;
 
 /*
@@ -199,6 +206,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
+	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
 };
 
 /*
